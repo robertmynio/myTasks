7c88798bfef5c5c2f2cfca0fc7bdb5fec8cda5a9
Robert Mynio <robert.mynio@ni.com>
2017-12-17 22:20:14 +0200

    Added Model, In-memory Repository, Controller with GetAll, GetById and Create methods. No frontend yet. Tested using Postman

---
+++ myTasks/myTasks/Persistence/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using myTasks.Models;
+using System.Collections.Generic;
+
+namespace myTasks.Persistence
+{
+    public interface ITaskRepository
+    {
+        IEnumerable<TaskItem> GetAll();
+        void Add(TaskItem item);
+        TaskItem Find(long id);
+    }
+}
