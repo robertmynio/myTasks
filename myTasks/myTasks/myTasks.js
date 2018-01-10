e236921a70630e335fbf085fc13372960ff5b99c
Robert Mynio <robert.mynio@ni.com>
2018-01-10 21:46:28 +0200

    Cleaned up initial HTML and JavaScript files

---
+++ myTasks/myTasks/myTasks.js
@@ -0,0 +1,13 @@
+﻿angular.module('MyTasksApp', [
+    'officeuifabric.core',
+    'officeuifabric.components'
+    ]).controller('MyTasksController', function ($http) {
+        var myTasks = this;
+
+        var onTaskComplete = function (response) {
+            myTasks.taskWithId1 = response.data;
+        }
+
+        $http.get("http://localhost:57600/api/tasks/1")
+            .then(onTaskComplete);
+    });
